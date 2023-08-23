120f05a3d98b15a57d1250022d1abe3c0af15632
Fran <francisco20990@gmail.com>
2023-08-23 09:12:01 -0400

    Migration data principal

--- database/migrations/2023_08_18_161426_create_data_table.php
+++ database/migrations/2023_08_18_161426_create_data_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('data', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('website');
             $table->timestamps();
         });
     }
